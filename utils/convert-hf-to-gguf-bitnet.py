7d98cbb951f9c784a40a98a4c6d5ab2e1f87ee2d
Edward Kim <edwardkim@Edwards-MacBook-Pro-2.local>
2026-01-28 21:17:23 -0800

    fixed vocab size mismatch error

--- utils/convert-hf-to-gguf-bitnet.py
+++ utils/convert-hf-to-gguf-bitnet.py
@@ -1105,6 +1105,9 @@ class QwenModel(BitnetModel):
 
         assert len(tokens) == vocab.vocab_size
 
+        # Set vocab size to the actual number of tokens extracted
+        self.gguf_writer.add_vocab_size(len(tokens))
+
         self.gguf_writer.add_tokenizer_model("gpt2")
         self.gguf_writer.add_tokenizer_pre("qwen2")
         self.gguf_writer.add_token_list(tokens)
